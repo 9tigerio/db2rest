8f15517fccd73fcd525ee38cf1dc434fc969d4ea
grabdoc <grabdoc2020@gmail.com>
2023-12-16 00:17:14 -0600

    fix #8

--- src/main/java/com/homihq/db2rest/rsql/RsqlTestCommandLineRunner.java
+++ src/main/java/com/homihq/db2rest/rsql/RsqlTestCommandLineRunner.java
@@ -5,13 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 //@Component
 @RequiredArgsConstructor
 @Slf4j
 public class RsqlTestCommandLineRunner implements CommandLineRunner {
 
     private final FilterBuilderService filterBuilderService;
-
+    private final DataSource dataSource;
     @Override
     public void run(String... args) throws Exception {
         String tableName = "actors";
@@ -21,5 +23,9 @@ public class RsqlTestCommandLineRunner implements CommandLineRunner {
                 filterBuilderService.getWhereClause(tableName, rSql);
 
         log.info("whereClause = {}", whereClause);
+        String dbName =
+        dataSource.getConnection().getMetaData().getDatabaseProductName();
+
+        log.info("dbName = {}", dbName);
     }
 }
