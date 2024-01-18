2b5b7789c1ee72d6fb03cfd904d4e57f02eb9b1a
grabdoc <grabdoc2020@gmail.com>
2024-01-18 01:10:18 -0600

    #154 - simplification of installer

--- src/main/java/com/homihq/db2rest/rest/create/bulk/CSVDataProcessor.java
+++ src/main/java/com/homihq/db2rest/rest/create/bulk/CSVDataProcessor.java
@@ -2,7 +2,6 @@ package com.homihq.db2rest.rest.create.bulk;
 
 
 import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
