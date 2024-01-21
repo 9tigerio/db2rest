031d1999717a09df97a08c3ad67d5de584b459d4
grabdoc <grabdoc2020@gmail.com>
2024-01-20 23:00:10 -0600

    fix #162 - fix sort issue

---
+++ src/main/java/com/homihq/db2rest/rest/read/model/DbAlias.java
@@ -0,0 +1,3 @@
+package com.homihq.db2rest.rest.read.model;
+
+public record DbAlias(String name, String alias) { }
---
+++ src/main/java/com/homihq/db2rest/rest/read/model/DbColumn.java
@@ -0,0 +1,7 @@
+package com.homihq.db2rest.rest.read.model;
+
+
+public record DbColumn(String tableName, String name, String alias) {
+
+
+}
