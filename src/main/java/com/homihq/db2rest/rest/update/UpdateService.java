a0fac681ab2037e84e141776ba8bcaaa5a63f529
bharath8121 <bharathchandra.chandra96@gmail.com>
2024-01-28 15:56:01 +0000

    updated update logic and added tests

--- src/main/java/com/homihq/db2rest/rest/update/UpdateService.java
+++ src/main/java/com/homihq/db2rest/rest/update/UpdateService.java
@@ -1,6 +1,7 @@
 package com.homihq.db2rest.rest.update;
 
 import com.homihq.db2rest.config.Db2RestConfigProperties;
+import com.homihq.db2rest.exception.GenericDataAccessException;
 import com.homihq.db2rest.exception.InvalidTableException;
 import com.homihq.db2rest.mybatis.MyBatisTable;
 import com.homihq.db2rest.rsql.operators.SimpleRSQLOperators;
@@ -8,6 +9,7 @@ import com.homihq.db2rest.rsql.parser.MyBatisFilterVisitor;
 import com.homihq.db2rest.schema.SchemaManager;
 import cz.jirutka.rsql.parser.RSQLParser;
 import cz.jirutka.rsql.parser.ast.Node;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -16,6 +18,8 @@ import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.update.UpdateDSL;
 import org.mybatis.dynamic.sql.update.UpdateModel;
 import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import schemacrawler.schema.Table;
@@ -28,21 +32,46 @@ import static org.mybatis.dynamic.sql.update.UpdateDSL.update;
 public class UpdateService {
 
     private final Db2RestConfigProperties db2RestConfigProperties;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final SchemaManager schemaManager;
     @Transactional
-    public void patch(String schemaName, String tableName, Map<String,Object> data, String filter) {
-        db2RestConfigProperties.verifySchema(schemaName);
+    public int patch(String schemaName, String tableName, Map<String,Object> data, String filter) {
 
-        Table t = schemaManager.getTable(schemaName, tableName)
-                .orElseThrow(() -> new InvalidTableException(tableName));
+        MyBatisTable table;
+        if(db2RestConfigProperties.getMultiTenancy().isSchemaBased()) {
+            table = schemaManager.getOneTable(schemaName, tableName);
+        } else {
+            table = schemaManager.getTable(tableName);
+        }
 
-        MyBatisTable table = new MyBatisTable(schemaName, tableName, t);
+        return executeUpdate(filter, data, table);
 
+    }
+
+    private int executeUpdate(String filter, Map<String, Object> data, MyBatisTable table) {
         UpdateDSL<UpdateModel> updateDSL = update(table);
 
         for(String key : data.keySet()) {
             updateDSL.set(table.column(key)).equalToOrNull(data.get(key));
         }
+
+        addWhere(filter, table, updateDSL);
+
+        UpdateStatementProvider updateStatement = updateDSL.build()
+                .render(RenderingStrategies.SPRING_NAMED_PARAMETER);
+
+        log.info("SQL - {}", updateStatement.getUpdateStatement());
+        log.info("Bind variables - {}", updateStatement.getParameters());
+
+        try {
+            return namedParameterJdbcTemplate.update(updateStatement.getUpdateStatement(),
+                    updateStatement.getParameters());
+        } catch (DataAccessException e) {
+            throw new GenericDataAccessException(e.getMostSpecificCause().getMessage());
+        }
+    }
+
+    private void addWhere(String filter, MyBatisTable table, UpdateDSL<UpdateModel> updateDSL) {
         if(StringUtils.isNotBlank(filter)) {
 
             Node rootNode = new RSQLParser(SimpleRSQLOperators.customOperators()).parse(filter);
@@ -52,16 +81,6 @@ public class UpdateService {
 
             updateDSL.where(condition);
         }
-
-        UpdateStatementProvider updateStatement = updateDSL.build()
-                .render(RenderingStrategies.SPRING_NAMED_PARAMETER);
-
-        String sql = updateStatement.getUpdateStatement();
-        Map<String,Object> bindValues = updateStatement.getParameters();
-
-
-        log.info("SQL - {}", sql);
-        log.info("Bind variables - {}", bindValues);
     }
 
 }
---
+++ src/test/java/com/homihq/db2rest/rest/PgSQLUpdateControllerTest.java
@@ -0,0 +1,97 @@
+package com.homihq.db2rest.rest;
+
+import com.homihq.db2rest.PostgreSQLBaseIntegrationTest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PgSQLUpdateControllerTest extends PostgreSQLBaseIntegrationTest {
+    @Test
+    @DisplayName("Update an existing film")
+    public void updateExistingFilm() throws Exception {
+
+        var json = """
+                {
+                    "rating": "NC-17"
+                }
+                """;
+
+        mockMvc.perform(patch("/film")
+                        .param("filter", "title==\"ACADEMY DINOSAUR\"")
+                        .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .header("Content-Profile", "sakila")
+                        .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rows", Matchers.equalTo(1)))
+                .andDo(print())
+                .andDo(document("pg-update-existing-film"));
+    }
+
+    @Test
+    @DisplayName("Update a non-existing film")
+    public void updateNonExistingFilm() throws Exception {
+
+        var json = """
+                {
+                    "special_features": "CGI,Fantasy,Action"
+                }
+                """;
+
+        mockMvc.perform(patch("/film")
+                        .param("filter", "title==\"BAAHUBALI\"")
+                        .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .header("Content-Profile", "sakila")
+                        .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rows", Matchers.equalTo(0)))
+                .andDo(print())
+                .andDo(document("pg-update-non-existing-film"));
+    }
+
+    @Test
+    @DisplayName("Update non-existing table")
+    public void updateNonExistingTable() throws Exception {
+        var json = """
+                {
+                    "sample_col": "sample value"
+                }
+                """;
+
+        mockMvc.perform(patch("/unnamed_table")
+                        .param("filter", "sample_col==\"sample value 1\"")
+                        .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .header("Content-Profile", "sakila")
+                        .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andDo(document("pg-update-non-existing-table"));
+    }
+
+    @Test
+    @DisplayName("Updating multiple films")
+    public void updateMultipleColumns() throws Exception {
+        var json = """
+                {
+                    "rating": "PG"
+                }
+                """;
+
+        mockMvc.perform(patch("/film")
+                        .param("filter", "rating==\"G\"")
+                        .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .header("Content-Profile", "sakila")
+                        .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.rows", Matchers.equalTo(2)))
+                .andDo(print())
+                .andDo(document("pg-update-multiple-films"));
+    }
+
+}
