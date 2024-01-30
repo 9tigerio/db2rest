6c057ea87645f992c0594753006cccf57f71a1d8
grabdoc <grabdoc2020@gmail.com>
2024-01-30 11:51:44 -0600

    #213 - refactored create controller

---
+++ src/main/java/com/homihq/db2rest/rest/create/CreateRestApi.java
@@ -0,0 +1,17 @@
+package com.homihq.db2rest.rest.create;
+
+import com.homihq.db2rest.rest.create.dto.CreateResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+public interface CreateRestApi {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{tableName}")
+    CreateResponse save(@PathVariable String tableName,
+                        @RequestHeader(name = "Content-Profile") String schemaName,
+                        @RequestBody Map<String, Object> data,
+                        @RequestParam(name = "tsid", required = false) String tsid,
+                        @RequestParam(name = "tsidType", required = false, defaultValue = "number") String tsidType);
+}
