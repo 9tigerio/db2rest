f0bbfac9608b81ed88ba5ca5370b041f60def802
grabdoc <grabdoc2020@gmail.com>
2024-02-07 09:56:37 -0600

    #254 refactor find one query

---
+++ src/main/java/com/homihq/db2rest/rest/read/FindOneController.java
@@ -0,0 +1,30 @@
+package com.homihq.db2rest.rest.read;
+
+import com.homihq.db2rest.rest.read.dto.FindOneResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class FindOneController {
+
+    private final ReadService readService;
+
+    @GetMapping("/{tableName}/one")
+    public FindOneResponse findOne(@PathVariable String tableName,
+                                   @RequestParam(name = "select", required = false, defaultValue = "") String select,
+                                   @RequestParam(name = "filter", required = false, defaultValue = "") String filter) {
+
+
+        log.info("tableName - {}", tableName);
+        log.info("select - {}", select);
+        log.info("filter - {}", filter);
+
+        return this.readService.findOne(tableName, select, filter);
+    }
+
+
+}
