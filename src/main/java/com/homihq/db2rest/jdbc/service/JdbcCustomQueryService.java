fd6c2ac23a8fd13f6730f40b61ca343cbb85a763
grabdoc <grabdoc2020@gmail.com>
2024-03-26 18:05:08 -0500

    #367 - fix to jar running issues from command line -

--- src/main/java/com/homihq/db2rest/jdbc/service/JdbcCustomQueryService.java
+++ src/main/java/com/homihq/db2rest/jdbc/service/JdbcCustomQueryService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
 @RequiredArgsConstructor
 public class JdbcCustomQueryService implements CustomQueryService {
