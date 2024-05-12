f5485cc53e8400ad7e1b49e35b315bd31e4f2389
grabdoc <grabdoc2020@gmail.com>
2024-05-12 01:20:55 -0500

    #560 - remove datasource type

--- api-rest/src/main/java/com/homihq/db2rest/jdbc/config/JdbcConfiguration.java
+++ api-rest/src/main/java/com/homihq/db2rest/jdbc/config/JdbcConfiguration.java
@@ -41,6 +41,7 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Configuration
@@ -68,6 +69,8 @@ public class JdbcConfiguration {
 
     private Map<Object, Object> buildDataSources() {
         final Map<Object, Object> result = new HashMap<>();
+
+        if(Objects.isNull(databaseProperties.getDatabases())) return result;
 
         for (Map<String,String> db : databaseProperties.getDatabases()) {
             result.put(db.get("name"), this.buildDataSource(db));
