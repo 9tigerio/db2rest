2c18ba55cec0a420c857db5ca37d17832394d833
grabdoc <grabdoc2020@gmail.com>
2024-05-12 13:48:06 -0500

    cleanup parameters

--- rest-common/src/main/java/com/homihq/db2rest/multidb/DatabaseConnectionDetail.java
+++ rest-common/src/main/java/com/homihq/db2rest/multidb/DatabaseConnectionDetail.java
@@ -2,7 +2,10 @@ package com.homihq.db2rest.multidb;
 
 import org.apache.commons.lang3.StringUtils;
 
-public record DatabaseConnectionDetail(String name, String type, String url, String username, String password, String database) {
+import java.util.Map;
+
+public record DatabaseConnectionDetail(String name, String type, String url, String username, String password, String database
+    ,Map<String,String> connectionProperties, EnvironmentProperties envProperties) {
     public boolean isMongo() {
         return StringUtils.equalsIgnoreCase(type, "MONGO");
     }
