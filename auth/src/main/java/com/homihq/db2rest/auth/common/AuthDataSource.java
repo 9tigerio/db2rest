b6d6405c938cad70d965fdbcb4bc4259153931ac
Lenur Serveriiev <lenur.serveriev@gmail.com>
2024-09-14 14:54:06 +0200

    Return AuthDataSource.excludedResources property empty list if not exist

--- auth/src/main/java/com/homihq/db2rest/auth/common/AuthDataSource.java
+++ auth/src/main/java/com/homihq/db2rest/auth/common/AuthDataSource.java
@@ -1,9 +1,15 @@
 package com.homihq.db2rest.auth.common;
 
-
 import java.util.List;
 
-public record AuthDataSource(String name, List<ResourceRole> resourceRoles,
-                             List<ApiExcludedResource> excludedResources, List<User> users) {
-
+public record AuthDataSource(
+        String name,
+        List<ResourceRole> resourceRoles,
+        List<ApiExcludedResource> excludedResources,
+        List<User> users
+) {
+    @Override
+    public List<ApiExcludedResource> excludedResources() {
+        return excludedResources == null ? List.of() : excludedResources;
+    }
 }
