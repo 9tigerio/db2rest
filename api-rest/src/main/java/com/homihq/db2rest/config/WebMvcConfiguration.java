bf47ae12613db58f749acfe253c0bac2277e9e65
ameya kulkarni <ameya.2000k@gmail.com>
2025-02-11 22:46:46 +0530

    Partially working, unable to get property from config, if value is hardcoded program works

--- api-rest/src/main/java/com/homihq/db2rest/config/WebMvcConfiguration.java
+++ api-rest/src/main/java/com/homihq/db2rest/config/WebMvcConfiguration.java
@@ -1,9 +1,15 @@
 package com.homihq.db2rest.config;
 
 import com.homihq.db2rest.interceptor.DatabaseContextRequestInterceptor;
+import com.homihq.db2rest.config.Db2RestConfigProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,11 +18,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Slf4j
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Autowired
+    Db2RestConfigProperties props;
+
 
     private final DatabaseContextRequestInterceptor databaseContextRequestInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(databaseContextRequestInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = Arrays.stream(props.getAllowedCorsList().split(","))
+                         .map(String::trim)
+                         .toArray(String[]::new);
+        for(String origin : origins){
+            System.out.println("*******************************************" + origin.toString() + "*****************************************************");
+
+        }
+        registry.addMapping("/**").allowedMethods("GET", "POST").allowedHeaders("*").allowedOrigins(origins);
     }
 }
