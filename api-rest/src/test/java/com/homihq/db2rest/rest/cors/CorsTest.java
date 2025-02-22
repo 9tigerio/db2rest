e03dcb8ca8cfd2eab1a9cb39bb3069a23c8ac650
ameya kulkarni <ameya.2000k@gmail.com>
2025-02-22 12:58:05 +0530

    Added testcases for cors

---
+++ api-rest/src/test/java/com/homihq/db2rest/rest/cors/CorsTest.java
@@ -0,0 +1,105 @@
+package com.homihq.db2rest.rest.cors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.options;
+
+import com.homihq.db2rest.config.Db2RestConfigProperties;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
+@Slf4j
+public class CorsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    protected ApplicationContext applicationContext;
+
+    @Autowired
+    protected Db2RestConfigProperties db2RestConfigProperties;
+
+    private static final List<String> postgresScripts = List.of("pg/postgres-sakila.sql",
+            "pg/postgres-sakila-data.sql", "pg/pg-sakila-functions.sql");
+
+    private static final PostgreSQLContainer testPostgres =
+            (PostgreSQLContainer) new PostgreSQLContainer("postgres:15.2-alpine")
+                    .withDatabaseName("postgres")
+                    .withUsername("postgres")
+                    .withPassword("postgres")
+                    .withReuse(true);
+    
+    static {
+        testPostgres.start();
+        var containerDelegate = new JdbcDatabaseDelegate(testPostgres, "");
+        postgresScripts.forEach(initScript -> ScriptUtils.runInitScript(containerDelegate, initScript));
+    }
+
+    // private final String testOrigin = "http://example.com";
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("DB_URL", () -> testPostgres.getJdbcUrl());
+        registry.add("DB_USER", () -> testPostgres.getUsername());
+        registry.add("DB_PASSWORD", () -> testPostgres.getPassword());
+        registry.add("CORS_ORIGIN_URL", () -> "http://db2rest.com");
+    }
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void shouldBeSuccessfullForCorrectCORSOrigin() throws Exception {
+        mockMvc.perform(options("http://localhost:8080/v1/rdbms/db/employee")
+                .header("Origin", "http://db2rest.com")
+                .header("Access-Control-Request-Method", "GET"))
+                .andExpect(status().isOk()) 
+                .andExpect(header().exists("Access-Control-Allow-Origin"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "http://db2rest.com"));
+    }
+    @Test
+    public void shouldBeFaliureForWrongCORSOrigin() throws Exception {
+        mockMvc.perform(options("http://localhost:8080/v1/rdbms/db/employee")
+                .header("Origin", "http://example.com/aws")
+                .header("Access-Control-Request-Method", "GET"))
+                .andExpect(status().isForbidden());
+    }
+    @Test
+    public void shouldBeFaliureForWrongCORSMethod() throws Exception {
+        mockMvc.perform(options("http://localhost:8080/v1/rdbms/db/employee")
+                .header("Origin", "http://example.com/aws")
+                .header("Access-Control-Request-Method", "PATCH"))
+                .andExpect(status().isForbidden());
+    }  
+}
