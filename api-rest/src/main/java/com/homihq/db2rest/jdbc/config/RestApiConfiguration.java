06c026cdf3d7b8e86e8c03096b8c27c04e55a26e
grabdoc <grabdoc2020@gmail.com>
2025-03-06 06:19:52 -0600

    #869 rename class for configuration order.

--- api-rest/src/main/java/com/homihq/db2rest/jdbc/config/RestApiConfiguration.java
+++ api-rest/src/main/java/com/homihq/db2rest/jdbc/config/RestApiConfiguration.java
@@ -18,19 +18,18 @@ import com.homihq.db2rest.jdbc.rest.rpc.ProcedureController;
 import com.homihq.db2rest.jdbc.rest.sql.SQLTemplateController;
 import com.homihq.db2rest.jdbc.rest.update.UpdateController;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import java.util.List;
 
 
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
-public class ApiConfiguration {
+public class RestApiConfiguration {
 
 
     //START ::: API
