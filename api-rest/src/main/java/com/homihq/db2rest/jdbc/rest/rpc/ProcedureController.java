26073930de470b1aae2e1605c2c6ee647ceb0ae8
kdhrubo78 <dhrubojyoti.kayal@camunda.com>
2025-07-16 23:13:32 +0530

    #890 - allow setting resultset name for stored procedure.

--- api-rest/src/main/java/com/homihq/db2rest/jdbc/rest/rpc/ProcedureController.java
+++ api-rest/src/main/java/com/homihq/db2rest/jdbc/rest/rpc/ProcedureController.java
@@ -4,12 +4,9 @@ import com.homihq.db2rest.jdbc.core.service.ProcedureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 import static com.homihq.db2rest.jdbc.rest.RdbmsRestApi.VERSION;
@@ -26,8 +23,9 @@ public class ProcedureController {
     public ResponseEntity<Map<String, Object>> execute(
             @PathVariable String dbId,
             @PathVariable String procName,
-            @RequestBody Map<String, Object> inParams) {
+            @RequestBody Map<String, Object> inParams,
+            @RequestParam(name = "resultSetKeys", required = false) List<String> resultSetKeys) {
         log.debug("Execute stored procedure {} with IN params {}", procName, inParams.entrySet());
-        return ResponseEntity.ok(procedureService.execute(dbId, procName, inParams));
+        return ResponseEntity.ok(procedureService.execute(dbId, procName, inParams,resultSetKeys));
     }
 }
