f3f8c11ec8466dade5421038738568c626954eaa
JEEVEEKA <jeeveeka.ps@kalvium.community>
2025-07-27 19:26:44 +0530

    Added JdbcTemplate bean to test configuration for integration tests

--- db2rest-dialects/db2-dialect/src/test/java/com/db2rest/jdbc/dialect/TestConfiguration.java
+++ db2rest-dialects/db2-dialect/src/test/java/com/db2rest/jdbc/dialect/TestConfiguration.java
@@ -4,9 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@TestPropertySource(properties = {
+    "spring.datasource.driver-class-name=com.ibm.db2.jcc.DB2Driver"
+})
 public class TestConfiguration {
 
     public static void main(String[] args) {
@@ -21,5 +29,16 @@ public class TestConfiguration {
     @Bean
     public DB2RestDB2Dialect db2RestDB2Dialect(ObjectMapper objectMapper) {
         return new DB2RestDB2Dialect(objectMapper);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        // This will be configured by the integration test via @DynamicPropertySource
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
