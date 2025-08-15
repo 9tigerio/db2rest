e6c098b99a2760186183613287b1fce34c58abda
ameya kulkarni <ameya.2000k@gmail.com>
2025-08-15 13:29:58 +0530

    Added transaction template to persist transaction

--- rdbms-support/src/main/java/com/homihq/db2rest/jdbc/core/service/JdbcProcedureService.java
+++ rdbms-support/src/main/java/com/homihq/db2rest/jdbc/core/service/JdbcProcedureService.java
@@ -9,25 +9,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 
 @Slf4j
 @RequiredArgsConstructor
+@Service
 public class JdbcProcedureService implements ProcedureService {
 
     private final JdbcManager jdbcManager;
 
     @Override
     public Map<String, Object> execute(String dbId, String subRoutineName, Map<String, Object> inParams) {
-        JdbcTemplate jdbcTemplate = jdbcManager.getNamedParameterJdbcTemplate(dbId).getJdbcTemplate();
-        Dialect dialect = jdbcManager.getDialect(dbId);
+        TransactionTemplate transactionTemplate = jdbcManager.getTxnTemplate(dbId);
+        
+        return transactionTemplate.execute(status -> {
+            try {
+                JdbcTemplate jdbcTemplate = jdbcManager.getNamedParameterJdbcTemplate(dbId).getJdbcTemplate();
+                Dialect dialect = jdbcManager.getDialect(dbId);
 
-        log.debug("Dialect selected: {}", dialect);
-        log.debug("inParams: {}", inParams);
+                log.debug("Dialect selected: {}", dialect);
+                log.debug("inParams: {}", inParams);
 
-        return doExecuteInternal(jdbcTemplate, subRoutineName, inParams);
+                Map<String, Object> res = doExecuteInternal(jdbcTemplate, subRoutineName, inParams);
+                
+                return res;
+            } catch (Exception e) {
+                status.setRollbackOnly();
+                log.error("Error executing procedure: {}", e.getMessage(), e);
+                throw e;
+            }
+        });
     }
 
     private Map<String, Object> doExecuteInternal(JdbcTemplate jdbcTemplate,
@@ -35,7 +51,7 @@ public class JdbcProcedureService implements ProcedureService {
         jdbcTemplate.setResultsMapCaseInsensitive(true);
 
         try {
-            return new SimpleJdbcCall(jdbcTemplate).withProcedureName(subRoutineName).execute(inParams);
+             return new SimpleJdbcCall(jdbcTemplate).withProcedureName(subRoutineName).execute(inParams);
         } catch (InvalidDataAccessApiUsageException ex) {
             throw new RpcException(subRoutineName, inParams);
         }
