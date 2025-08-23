b23b49f050b4e69ee6f9e5ed897cb441b3dcf9d6
ameya kulkarni <ameya.2000k@gmail.com>
2025-08-23 14:59:39 +0530

    Added tests for MySQL

--- db2rest-api/api-rest/src/test/resources/mysql/mysql-sakila.sql
+++ db2rest-api/api-rest/src/test/resources/mysql/mysql-sakila.sql
@@ -260,6 +260,12 @@ CREATE PROCEDURE GetMovieRentalRateProc(IN movieTitle varchar(100), OUT rentalRa
         SELECT rental_rate INTO rentalRate FROM film WHERE title = movieTitle;
     END;
 
+CREATE PROCEDURE UpdateUserProc(IN user_id INT, OUT ROW_COUNT INT)
+    BEGIN
+        UPDATE users SET isActive = 0 WHERE auid = user_id;
+        SET ROW_COUNT = ROW_COUNT();
+    END;
+
 --
 -- Function
 --
@@ -272,4 +278,15 @@ BEGIN
         SET rentalRate = 0.00;
         SELECT rental_rate INTO rentalRate FROM film WHERE title = movieTitle;
         RETURN (rentalRate);
+END;
+
+CREATE FUNCTION UpdateUserFunc(user_id INT)
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+        DECLARE ROW_COUNT INT;
+        SET ROW_COUNT = 0;
+        UPDATE users SET isActive = 0 WHERE auid = user_id;
+        SET ROW_COUNT = ROW_COUNT();
+        RETURN (ROW_COUNT);
 END;
