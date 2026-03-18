a1e24909609ac2bfa43a7009acd01463c8b06fa3
Akanksha Raut <wn641065@dal.ca>
2026-03-18 01:57:54 -0300

    refactor(db2rest-core): make validators map unmodifiable in CustomPlaceholderValidators

--- db2rest-core/rdbms-support/src/main/java/com/homihq/db2rest/jdbc/validator/CustomPlaceholderValidators.java
+++ db2rest-core/rdbms-support/src/main/java/com/homihq/db2rest/jdbc/validator/CustomPlaceholderValidators.java
@@ -3,6 +3,7 @@ package com.homihq.db2rest.jdbc.validator;
 import com.homihq.db2rest.jdbc.validator.impl.IsRequiredValidator;
 import com.homihq.db2rest.jdbc.validator.impl.IsUUIDValidator;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,14 +11,20 @@ public class CustomPlaceholderValidators {
     public static final String IS_REQUIRED = "is_required";
     public static final String IS_UUID = "is_uuid";
 
-    private final Map<String, ConstraintValidator> validators = new HashMap<>();
+    private final Map<String, ConstraintValidator> validators;
 
     public CustomPlaceholderValidators() {
-        validators.put(IS_REQUIRED, new IsRequiredValidator());
-        validators.put(IS_UUID, new IsUUIDValidator());
+        Map<String, ConstraintValidator> map = new HashMap<>();
+        map.put(IS_REQUIRED, new IsRequiredValidator());
+        map.put(IS_UUID, new IsUUIDValidator());
+        this.validators = Collections.unmodifiableMap(map);
     }
 
     public ConstraintValidator getValidator(String constraint) {
         return validators.get(constraint);
+    }
+
+    public Map<String, ConstraintValidator> getValidators() {
+        return validators;
     }
 }
